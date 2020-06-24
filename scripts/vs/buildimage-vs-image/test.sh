c8005fab9467665d4eb3bb2d86abe136a44c3bd9
Danny Allen <daall@microsoft.com>
2020-06-24 10:43:51 -0700

    Support multiple testbeds for KVM test nodes

--- scripts/vs/buildimage-vs-image/test.sh
+++ scripts/vs/buildimage-vs-image/test.sh
@@ -5,8 +5,30 @@ echo ${JOB_NAME##*/}.${BUILD_NUMBER}
 ls -l
 virsh -c qemu:///system list
 
-tbname=vms-kvm-t0
-dut=vlab-01
+( # START OF CRITICAL SECTION
+
+# NOTE: Jenkins currently limits each worker to 2 KVM jobs at once. So, we only need
+# one lock to maintain consistency. The logic is:
+#
+# - If we are able to grab the lock, then vms-kvm-t0 is not in use. So, we are free
+#   to use this testbed, regardless of whether vms-kvm-t0-2 is currently being used
+#   or not.
+#
+# - If we are not able to grab the lock, then vms-kvm-t0 is in use. Since we are limited
+#   to 2 jobs per node, that means vms-kvm-t0-2 is not currently in use, so we are free
+#   to use that testbed.
+#
+# This logic may need to be refined as we add more testbeds, but it should work for now.
+TESTBED_1_LOCK_HELD=0
+flock -xn 200 || TESTBED_1_LOCK_HELD=$?
+
+if [ $TESTBED_1_LOCK_HELD -eq 0 ] ; then
+    tbname=vms-kvm-t0
+    dut=vlab-01
+else
+    tbname=vms-kvm-t0-2
+    dut=vlab-04
+fi
 
 docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWD sonicdev-microsoft.azurecr.io:443
 docker pull sonicdev-microsoft.azurecr.io:443/docker-sonic-mgmt:latest
@@ -45,3 +67,5 @@ if [ $? != 0 ]; then
     fi
     exit 2
 fi
+
+) 200>/var/lock/kvm_lock # END OF CRITICAL SECTION
